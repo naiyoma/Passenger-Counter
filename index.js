8b4a321594eb3dae9f949e196e01484edd238091
naiyoma <lankas.aurelia@gmail.com>
2022-02-08 21:38:33 +0300

    recet counter

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let count = 0;
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 function increment () {
-    count = count + 1
-    document.getElementById('count-el').innerText=count;
+    count += 1
+    countEl.textContent=count;
+}
+function save() {
+    let countStr = count + "-"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
