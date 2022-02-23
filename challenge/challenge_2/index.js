b50e319cba7727c6b618e40845d049f1d84ac096
naiyoma <lankas.aurelia@gmail.com>
2022-02-23 19:36:26 +0300

    home page images

---
+++ challenge/challenge_2/index.js
@@ -0,0 +1,39 @@
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+let nextButton = document.getElementById('carousel-button-next');
+let prevButton = document.getElementById('carousel-button-prev')
+
+nextButton.addEventListener('click', moveToNextSlide);
+prevButton.addEventListener('click', moveToPrevSlide);
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden')
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition ++;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+
+    if(slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
