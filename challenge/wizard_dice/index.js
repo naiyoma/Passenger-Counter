26e29095b9feaf9d65cb47e8048ce1490fd4d2f1
naiyoma <lankas.aurelia@gmail.com>
2022-02-26 20:39:59 +0300

    chanllenge 3 re-do

--- challenge/wizard_dice/index.js
+++ challenge/wizard_dice/index.js
@@ -3,7 +3,8 @@ const hero = {
     name: "Wizard",
     avatar: "images/ciri1.jpg",
     health: "60",
-    dice: 6
+    dice: [3,56, 4],
+    diceCount: 3
 }
 
 const monster = {
@@ -11,13 +12,22 @@ const monster = {
     name: "Orc",
     avatar: "images/ciri2.jpg",
     health: "10",
-    dice: 4
+    dice: [3,4],
+    diceCount: 1
 }
 
 
 
 function renderCharacter(data) {
-    const {id, name, avatar, health, dice} = data;
+    const {id, name, avatar, health, dice, diceCount} = data;
+
+    const diceHtml = dice.map(function(x) {
+        return `<div class="dice">${dice[x]}</div>`
+    })
+    // let diceHtm = ''
+    // for (let i =0; i<diceCount; i++){
+    //     diceHtm += `<div class="dice">${dice[i]}</div>`
+    // }
     document.getElementById(id).innerHTML = `
         <div class="character-card">
             <h4 class="name"> ${name} </h4>
