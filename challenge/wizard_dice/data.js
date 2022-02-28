37a14623a56e1a3e3833f15bd8de9c5736ee454f
naiyoma <lankas.aurelia@gmail.com>
2022-02-28 15:25:58 +0300

    export character data

---
+++ challenge/wizard_dice/data.js
@@ -0,0 +1,20 @@
+
+const CharacterData = {
+    hero: {
+        id: 'hero',
+        name: "Wizard",
+        avatar: "images/ciri1.jpg",
+        health: "60",
+        diceCount: 3,
+    },
+
+    monster: {
+        id: 'monster',
+        name: "Orc",
+        avatar: "images/ciri2.jpg",
+        health: "10",
+        diceCount: 1
+    }
+}
+
+export default CharacterData
--- challenge/wizard_dice/index.js
+++ challenge/wizard_dice/index.js
@@ -1,30 +1,19 @@
-const hero = {
-    id: 'hero',
-    name: "Wizard",
-    avatar: "images/ciri1.jpg",
-    health: "60",
-    diceCount: 3
-}
-
-const monster = {
-    id: 'monster',
-    name: "Orc",
-    avatar: "images/ciri2.jpg",
-    health: "10",
-    diceCount: 1
-}
+import CharacterData from './data.js'
 
 function Character(data) {
-    this.id = data.id
-    this.name = data.name
-    this.avatar = data.avatar
-    this.health = data.health
-    this.diceCount = data.diceCount
+    Object.assign(this, data)
+
+    this.getDiceHtml = function(diceCount) {
+       return getDiceRollyArray(diceCount).map(function(num) {
+            return `<div class="dice">${num}</div>`
+        }).join(" ")
+    }
+
     this.getCharacterHtml = function() {
         const {id, name, avatar, health, diceCount} = this;
-        const diceHtml = getDiceHtml(diceCount)
+        const diceHtml = this.getDiceHtml(diceCount)
 
-        document.getElementById(id).innerHTML = `
+        return `
         <div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}"/>
@@ -35,10 +24,19 @@ function Character(data) {
         </div>`;
     }
 }
-const wizard = new Character(hero)
-const orc = new Character(monster)
-wizard.getCharacterHtml()
-orc.getCharacterHtml()
+const wizard = new Character(CharacterData.hero)
+const orc = new Character(CharacterData.monster)
+
+
+function render() {
+    document.getElementById(wizard.id).innerHTML = wizard.getCharacterHtml()
+    document.getElementById(orc.id).innerHTML = orc.getCharacterHtml()
+    wizard.getCharacterHtml()
+    orc.getCharacterHtml()
+}
+
+render()
+
 
 
 
@@ -49,8 +47,3 @@ function getDiceRollyArray(diceCount) {
     return diceArray
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRollyArray(diceCount).map(function(num) {
-        return `<div class="dice">${num}</div>`
-    }).join(" ")
-}
