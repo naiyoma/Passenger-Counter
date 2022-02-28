ba878b61b70a1aad5b8d4cfda75d3ea432cb1485
naiyoma <lankas.aurelia@gmail.com>
2022-02-28 13:22:06 +0300

    last draw function

--- challenge/wizard_dice/index.js
+++ challenge/wizard_dice/index.js
@@ -3,7 +3,6 @@ const hero = {
     name: "Wizard",
     avatar: "images/ciri1.jpg",
     health: "60",
-    dice: [3,56, 4],
     diceCount: 3
 }
 
@@ -12,31 +11,36 @@ const monster = {
     name: "Orc",
     avatar: "images/ciri2.jpg",
     health: "10",
-    dice: [3,4],
     diceCount: 1
 }
 
+function getDiceRollyArray(diceCount) {
+    let diceArray = new Array(diceCount).fill(0).map(function(){
+        return Math.floor(Math.random() * 6) + 1
+    })
+    return diceArray
+}
 
+function getDiceHtml(diceCount) {
+    return getDiceRollyArray(diceCount).map(function(num) {
+        return `<div class="dice">${num}</div>`
+    }).join(" ")
+}
 
 function renderCharacter(data) {
-    const {id, name, avatar, health, dice, diceCount} = data;
-
-    const diceHtml = dice.map(function(x) {
-        return `<div class="dice">${dice[x]}</div>`
-    })
-    // let diceHtm = ''
-    // for (let i =0; i<diceCount; i++){
-    //     diceHtm += `<div class="dice">${dice[i]}</div>`
-    // }
+    const {id, name, avatar, health, diceCount} = data;
+    const diceHtml = getDiceHtml(diceCount)
     document.getElementById(id).innerHTML = `
         <div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}"/>
             <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container"><div class="dice"> ${dice} </div></div>
-        </div>
-    `;
+            <div class="dice-container">
+                 ${diceHtml}
+            </div>
+        </div>`;
 }
 
 renderCharacter(hero)
 renderCharacter(monster)
+
